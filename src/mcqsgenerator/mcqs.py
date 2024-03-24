f9cf6b2984627c64607aded295b2d724c8fe8acd
hamza <y7hamzakhanswati@gmail.com>
2024-03-24 22:19:53 +0500

    file are added

--- src/mcqsgenerator/mcqs.py
+++ src/mcqsgenerator/mcqs.py
@@ -2,14 +2,11 @@ import os
 import json
 import pandas as pd
 import traceback
-from langchain.chat_models import ChatOpenAI
 from dotenv import load_dotenv
 from langchain import HuggingFaceHub
-from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chains import SequentialChain
-from langchain.callbacks import get_openai_callback
 import PyPDF2
 
 
